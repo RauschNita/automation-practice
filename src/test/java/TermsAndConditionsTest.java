88e38b52bbc04ecb87c18a77830baffc9e330dd6
rausch.nita <rausch.nita@gmail.com>
2021-08-25 01:56:16 +0200

    terms and conditions text wrote into a txt test is done

--- src/test/java/TermsAndConditionsTest.java
+++ src/test/java/TermsAndConditionsTest.java
@@ -4,10 +4,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.io.*;
+import java.util.List;
 
 public class TermsAndConditionsTest extends Utils{
     WebDriver driver;
+
+    private final By TERMS_CONS = By.xpath("//*[@id=\"block_various_links_footer\"]/ul/li[6]/a");
+    private final By TERMS_AND_CONDITIONS_TEXT = By.cssSelector("#columns .row");
 
     @BeforeEach
     public void setUp() {
@@ -16,14 +23,31 @@ public class TermsAndConditionsTest extends Utils{
     }
 
     @Test
-    public void termsAndConditions() {
-        final By TERMS_CONS = By.xpath("//*[@id=\"block_various_links_footer\"]/ul/li[6]/a");
+    public void termsAndConditions() throws IOException {
 
         getWebDriverWait().until(ExpectedConditions.elementToBeClickable((TERMS_CONS))).click();
 
-        String result = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/h1")).getText();
-        String expected = "TERMS AND CONDITIONS OF USE";
-        Assertions.assertEquals(expected, result);
+        FileWriter writer = new FileWriter("termsandconditions.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(writer);
+
+        List<WebElement> texts = driver.findElements(TERMS_AND_CONDITIONS_TEXT);
+        for (WebElement text : texts) {
+            bufferedWriter.append(text.getText());
+        }
+        bufferedWriter.close();
+
+        int lines = 0;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("termsandconditions.txt"));
+            while (reader.readLine() != null) lines++;
+            reader.close();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+
+        Assertions.assertEquals(7, lines);
     }
 
     @AfterEach
